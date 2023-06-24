ff3d33928df8e157a236c7907d981fb271619b12
Sriramakrishnan S <sriramakrishnan005@gmail.com>
2023-06-24 17:57:49 +0530

    Initial commit

---
+++ src/com/kce/hbs/service/HotelBooking.java
@@ -0,0 +1,10 @@
+package com.kce.hbs.service;
+
+public class HotelBooking {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("------------------------------Hotel Booking System------------------------------");
+
+        }
+    }
+}
